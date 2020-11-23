9f3917420929d65a6c5d769c5866211c894d5d83
JakubGalaz <kubixes@interia.pl>
2020-11-23 22:57:13 +0100

    slider

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ap';
+
+
+  prev() {
+    document.getElementById('container').classList.add('margin-left')
+    console.log("perv");
+  }
+
+  dalej() {
+
+  }
 }
